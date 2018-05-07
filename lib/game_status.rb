f260111e2e810b865d7c8457118f68c3b76aa831
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-07 16:43:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,13 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
-    if board[win_combo[0]] == "X" &&
-      board[win_combo[1]] == "X" &&
-      board[win_combo[2]] == "X"
+    if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
       return win_combo
-    elsif board[win_combo[0]] == "O" &&
-      board[win_combo[1]] == "O" &&
-      board[win_combo[2]] == "O"
+    elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
       return win_combo
     end
   end
@@ -42,13 +38,5 @@ def over?(board)
 end
 
 def winner?(board)
-  if !won?
-    nil
-  else WIN_COMBINATIONS.detect do |win_combo|
-    if WIN_COMBINATIONS
-      "X"
-    elsif won?(board, 'O', win_combo)
-      "O"
-    end
-  end
+
 end
