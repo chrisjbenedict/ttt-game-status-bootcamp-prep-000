0a01085f934b352adb63dec745725a8d3aaca26d
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-07 17:01:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,8 +39,10 @@ def over?(board)
 end
 
 def winner?(board)
-  won = won?(board)
- if won != nil
-   return board[won[0]]
- end
+  if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
+    return "X"
+  elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
+    return "O"
+  end
+  false
 end
